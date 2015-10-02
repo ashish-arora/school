908fce85d695f7a7b923a06cd8d6cbf862e4374c
ashish-arora <ashish@hike.in>
2015-10-02 17:57:46 +0530

    added forms

---
+++ schoolapp/forms.py
@@ -0,0 +1,8 @@
+__author__ = 'ashish'
+from django import forms
+
+class OrganizationForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
