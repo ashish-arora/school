71d711b4d578f4a2ee3ea98b15426e12e4d85594
ashish-arora <ashish@hike.in>
2015-10-14 02:06:04 +0530

    Added logging in the settings

--- school/settings.py
+++ school/settings.py
@@ -87,6 +87,24 @@ CACHES = {
     },
 }
 
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'DEBUG',
+        }
+    },
+}
+
+
 REDIS_CONN = redis.StrictRedis()
 
 WSGI_APPLICATION = 'school.wsgi.application'
